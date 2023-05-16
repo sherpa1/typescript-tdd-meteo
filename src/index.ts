de5521f1c99911871f3801cc9fd8766c6dfa3a69
Alexandre Leroux <alex@sherpa.one>
2023-05-16 20:43:16 +0200

    index

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Math from "./classes/Math";
+
+const m: Math = new Math();
+
+const addResult: number = m.add(1, 1);
+console.log(addResult);
+
+const sumResult: number = m.sum(1, 1, 1);
+console.log(sumResult);
